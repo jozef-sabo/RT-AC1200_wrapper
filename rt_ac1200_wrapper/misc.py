d314d55dd9ec234ba48a94a0c23eb42751202a2d
Jozef Sabo <31158086+jozef-sabo@users.noreply.github.com>
2022-10-19 23:44:40 +0200

    Created authorization string creation function and tests

--- rt_ac1200_wrapper/misc.py
+++ rt_ac1200_wrapper/misc.py
@@ -1,9 +1,17 @@
 import re
+import base64
 
 
-def check_ip(ip: str) -> bool:
+def is_ip_valid(ip: str) -> bool:
     regex = "^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$"
 
     if re.search(regex, ip):
         return True
     return False
+
+
+def create_authorization_string(username: str, password: str) -> str:
+    # username is stripped (trim function), password is not (from Main_Login.asp login() function)
+    username = username.strip()
+    # Base64 encoded for authorization header (from Main_Login.asp login() function)
+    return base64.b64encode(bytes(f"{username}:{password}", encoding="utf-8")).decode("UTF-8")
--- tests/test_misc.py
+++ tests/test_misc.py
@@ -3,16 +3,31 @@ from rt_ac1200_wrapper import misc as misc
 
 
 class Tests(unittest.TestCase):
-    def test_check_ip(self) -> None:
-        self.assertTrue(misc.check_ip("192.168.1.1"))
-        self.assertTrue(misc.check_ip("0.0.0.0"))
-        self.assertTrue(misc.check_ip("1.1.0.0"))
-        self.assertTrue(misc.check_ip("255.255.255.255"))
-        self.assertFalse(misc.check_ip("256.255.255.255"))
-        self.assertFalse(misc.check_ip("255.256.255.255"))
-        self.assertFalse(misc.check_ip("255.256.256.255"))
-        self.assertFalse(misc.check_ip("255.255.255.256"))
-        self.assertFalse(misc.check_ip("255.255.255"))
-        self.assertFalse(misc.check_ip("255.255"))
-        self.assertFalse(misc.check_ip("a"))
-        self.assertFalse(misc.check_ip("localhost"))
+    def test_is_ip_valid(self) -> None:
+        self.assertTrue(misc.is_ip_valid("192.168.1.1"))
+        self.assertTrue(misc.is_ip_valid("0.0.0.0"))
+        self.assertTrue(misc.is_ip_valid("1.1.0.0"))
+        self.assertTrue(misc.is_ip_valid("255.255.255.255"))
+        self.assertFalse(misc.is_ip_valid("256.255.255.255"))
+        self.assertFalse(misc.is_ip_valid("255.256.255.255"))
+        self.assertFalse(misc.is_ip_valid("255.256.256.255"))
+        self.assertFalse(misc.is_ip_valid("255.255.255.256"))
+        self.assertFalse(misc.is_ip_valid("255.255.255"))
+        self.assertFalse(misc.is_ip_valid("255.255"))
+        self.assertFalse(misc.is_ip_valid("a"))
+        self.assertFalse(misc.is_ip_valid("localhost"))
+
+    def test_create_authorization_string(self) -> None:
+        self.assertEqual(misc.create_authorization_string("", ""), "Og==")
+        self.assertEqual(misc.create_authorization_string("username", "password"), "dXNlcm5hbWU6cGFzc3dvcmQ=")
+        self.assertEqual(misc.create_authorization_string("username ", "password"), "dXNlcm5hbWU6cGFzc3dvcmQ=")
+        self.assertEqual(misc.create_authorization_string(" username", "password"), "dXNlcm5hbWU6cGFzc3dvcmQ=")
+        self.assertEqual(misc.create_authorization_string(" username ", "password"), "dXNlcm5hbWU6cGFzc3dvcmQ=")
+
+        self.assertNotEqual(misc.create_authorization_string("username", " password"), "dXNlcm5hbWU6cGFzc3dvcmQ=")
+        self.assertNotEqual(misc.create_authorization_string("username", "password "), "dXNlcm5hbWU6cGFzc3dvcmQ=")
+        self.assertNotEqual(misc.create_authorization_string("username", " password "), "dXNlcm5hbWU6cGFzc3dvcmQ=")
+
+        self.assertEqual(misc.create_authorization_string("012", "012"), "MDEyOjAxMg==")
+        self.assertEqual(misc.create_authorization_string("", "a"), "OmE=")
+        self.assertEqual(misc.create_authorization_string("a", ""), "YTo=")
