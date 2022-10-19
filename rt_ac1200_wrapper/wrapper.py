f3b2e8cc15cb9057d758c0a34ba58cf7269981b0
Jozef Sabo <31158086+jozef-sabo@users.noreply.github.com>
2022-10-19 23:45:10 +0200

    Added username and password to AC1200_router class

--- rt_ac1200_wrapper/wrapper.py
+++ rt_ac1200_wrapper/wrapper.py
@@ -3,16 +3,21 @@ from . import misc
 
 
 class AC1200_router(object):
-    def __init__(self, ip: str, port: int = 80):
+    def __init__(self, ip: str, username: str, password: str, port: int = 80):
         ip = ip.strip().strip("/")
         if ip == "localhost":
             ip = "127.0.0.1"
-        if not misc.check_ip(ip):
+        if not misc.is_ip_valid(ip):
             raise ValueError("Inserted IP is not valid")
         self.ip = ip
 
         assert 65_535 >= port > 0, "Port is not between 1-65535"
         self.port = port
+
+        assert username, "Username was not set"
+        assert password, "Password was not set"
+        self.authorization = misc.create_authorization_string(username, password)
+
 
 
 
--- tests/test_wrapper.py
+++ tests/test_wrapper.py
@@ -4,20 +4,27 @@ from rt_ac1200_wrapper import AC1200_router
 
 class Tests(unittest.TestCase):
     def test_AC1200_router_class(self) -> None:
-        self.assertTrue(AC1200_router("192.168.1.1"))
-        self.assertTrue(AC1200_router("192.168.1.1", 1))
-        self.assertTrue(AC1200_router("192.168.1.1", 65535))
-        self.assertTrue(AC1200_router("localhost"))
+        self.assertTrue(AC1200_router("192.168.1.1", "username", "password"))
+        self.assertTrue(AC1200_router("192.168.1.1", "username", "password", 1))
+        self.assertTrue(AC1200_router("192.168.1.1", "username", "password", 65535))
+        self.assertTrue(AC1200_router("localhost", "username", "password"))
 
         with self.assertRaises(ValueError):
-            AC1200_router("256.255.255.0")
+            AC1200_router("256.255.255.0", "username", "password")
 
         with self.assertRaises(AssertionError):
-            AC1200_router("192.168.1.1", 0)
+            AC1200_router("localhost", "", "")
         with self.assertRaises(AssertionError):
-            AC1200_router("192.168.1.1", 65536)
+            AC1200_router("localhost", "a", "")
         with self.assertRaises(AssertionError):
-            AC1200_router("192.168.1.1", -1)
+            AC1200_router("localhost", "", "a")
+
+        with self.assertRaises(AssertionError):
+            AC1200_router("192.168.1.1", "username", "password", 0)
+        with self.assertRaises(AssertionError):
+            AC1200_router("192.168.1.1", "username", "password", 65536)
+        with self.assertRaises(AssertionError):
+            AC1200_router("192.168.1.1", "username", "password", -1)
 
 
 
